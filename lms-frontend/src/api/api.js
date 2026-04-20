c2573f97ce13bad081f895b09101c0c9f0a08420
Rutvik Mathapati <ruties4354@gmail.com>
2026-04-20 17:26:37 +0530

    frontend production api url

--- lms-frontend/src/api/api.js
+++ lms-frontend/src/api/api.js
@@ -7,7 +7,7 @@ import axios from "axios";
  */
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "http://cbs-1.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
